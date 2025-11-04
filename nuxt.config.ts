7fc2d0f5f947c4cf30df34f20c7faf006eb572e0
Michael Babaiants <mihailbab13@gmail.com>
2025-11-04 21:53:40 +0300

    Added tailwind and several nuxt-config props.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import tailwindcss from '@tailwindcss/vite'
 
+export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils'],
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'], compatibilityDate: '2025-07-15',
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  ui: {
+    experimental: {
+      componentDetection: true,
+    },
+    theme: {
+      defaultVariants: {
+        color: 'neutral',
+      },
+    },
+  },
+  compatibilityDate: '2025-07-15',
+  vite: {
+    plugins: [tailwindcss()],
+  },
   eslint: {
     config: {
-      stylistic: true, // <---
+      stylistic: true,
     },
   },
 })
